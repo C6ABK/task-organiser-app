76f9eba5e828ac62ea23a58ab32395f02a1009b5
Adam Bark <40392203+C6ABK@users.noreply.github.com>
2025-09-02 17:53:16 +0100

    Facebook unban message!

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
     <main>
-      <h1>Task Organizer</h1>
-      <p>Organize your tasks efficiently</p>
+      <h1 className="text-3xl font-bold">FACEBOOK</h1>
+      <br />
+      <p>I believe I was banned for sharing a link over messenger for this website.</p> 
+        <p>It is a simple web project I was sharing with my friends. </p>
+        <p>Click here to view the Github repository... <a href="https://github.com/C6ABK/task-organiser-app">https://github.com/C6ABK/task-organiser-app</a></p>
+      <br />
+      <p>-Adam Bark</p>
     </main>
   )
 }
